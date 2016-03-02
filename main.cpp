a694dc022ba7c44b3cc6f3f01ec7ebd58c35b6c9
carlos93 <carlos93_fuenla@hotmail.com>
2016-03-02 20:09:50 +0100

    Improvements.

--- main.cpp
+++ main.cpp
@@ -11,17 +11,24 @@ void initFunc();
 void funReshape(int w, int h);
 void funDisplay();
 void drawTriangulo(GLfloat s, char color);
-void funKeyboard(int key, int x, int y);
+void funKeyboardUp(unsigned char key, int x, int y);
+void funSpecial(int key, int x, int y);
 void funMouse(int key, int state, int x, int y);
 void funMotion(int x, int y);
 void funMotionPassive(int x, int y);
+void funMouseWheel(int wheel, int direction, int x, int y);
 void drawNumber();
 void drawSphere(GLfloat radius, char color, bool wired = true);
 void drawPieza(GLfloat width, GLfloat height);
 void drawCube();
+void drawPlane(GLfloat width, GLfloat height);
 
 // Variables globales
 GLfloat desZ = -5.0, rotY = 0.0, rotX = 0.0, posX = 0.0, posY = 0.0;
+
+GLfloat cameraPos[3]    = { 0.0, 0.0, 5.0 };
+GLfloat lookat[3]       = { 0.0, 0.0, -5.0 };
+GLfloat up[3]           = { 0.0,  1.0,  0.0 };
 
 int oldX = 0, oldY = 0;
 
@@ -42,10 +49,13 @@ int main(int argc, char** argv) {
     // Configuración CallBacks
     glutReshapeFunc(funReshape);
     glutDisplayFunc(funDisplay);
-    glutSpecialFunc(funKeyboard);
+    glutKeyboardUpFunc(funKeyboardUp);
+    glutSpecialFunc(funSpecial);
     glutMouseFunc(funMouse);
     glutMotionFunc(funMotion);
-    glutPassiveMotionFunc(funMotionPassive);
+    //glutPassiveMotionFunc(funMotionPassive);
+    glutIdleFunc(funDisplay);
+    glutMouseWheelFunc(funMouseWheel);
 
     // Bucle principal
     glutMainLoop();
@@ -73,8 +83,6 @@ void initFunc() {
     glPolygonOffset(1.0, 1.0);
     glShadeModel(GL_FLAT);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-
 }
 
 void funReshape(int w, int h) {
@@ -88,58 +96,81 @@ void funReshape(int w, int h) {
     gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 3.0, 50.0);
 }
 
-void funKeyboard(int key, int x, int y) {
-
-    switch (key) {
-    case GLUT_KEY_UP:
-        rotX = rotX - 5.0f;
+void funKeyboardUp(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+        cameraPos[1] = cameraPos[1] + 1.0f;
+        lookat[1] = lookat[1] + 1.0f;
         break;
-    case GLUT_KEY_DOWN:
-        rotX = rotX + 5.0f;
+    case 's':
+        cameraPos[1] = cameraPos[1] - 1.0f;
+        lookat[1] = lookat[1] - 1.0f;
         break;
-    case GLUT_KEY_RIGHT:
-        rotY = rotY + 5.0f;
+    case 'a':
+        cameraPos[0] = cameraPos[0] - 1.0f;
+        lookat[0] = lookat[0] - 1.0f;
         break;
-    case GLUT_KEY_LEFT:
-        rotY = rotY - 5.0f;
+    case 'd':
+        cameraPos[0] = cameraPos[0] + 1.0f;
+        lookat[0] = lookat[0] + 1.0f;
         break;
-    default:
-        desZ = -5.0f;
-        rotY = 0.0f;
-        rotX = 0.0f;
+    case ' ':
+        cameraPos[2] = cameraPos[2] - 1.0f;
+        lookat[2] = lookat[2] - 1.0f;
         break;
     }
 
-    glutPostRedisplay();
+    printf("KEYBOARD: key: %c, x: %d, y: %d \n", key, x, y);
+}
+
+void funSpecial(int key, int x, int y)
+{
+
+    switch (key)
+    {
+    case GLUT_KEY_UP:
+        cameraPos[1] = cameraPos[1] + 1.0f;
+        lookat[1] = lookat[1] + 1.0f;
+        break;
+    case GLUT_KEY_DOWN:
+        cameraPos[1] = cameraPos[1] - 1.0f;
+        lookat[1] = lookat[1] - 1.0f;
+        break;
+    case GLUT_KEY_RIGHT:
+        cameraPos[0] = cameraPos[0] + 1.0f;
+        lookat[0] = lookat[0] + 1.0f;
+        break;
+    case GLUT_KEY_LEFT:
+        cameraPos[0] = cameraPos[0] - 1.0f;
+        lookat[0] = lookat[0] - 1.0f;
+        break;
+    default:
+        break;
+    }
+
+    //glutPostRedisplay();
+    printf("KEYBOARD SPECIAL: key: %d, x: %d, y: %d \n", key, x, y);
 }
 
 void funMouse(int key, int state, int x, int y)
 {
-    if (state == GLUT_UP)
-        return;
-
     oldX = x;
     oldY = y;
-    /*if (x > 1000 / 2.0f)
-        rotY += 5.0f;
-    else if (x < 1000 / 2.0f)
-        rotY -= 5.0f;
 
-    if (y > 500 / 2.0f)
-        rotX += 5.0f;
-    else if (y < 500.0f)
-        rotX -= 5.0f;
-
-    printf("x: %u, y: %u \n", x, y);*/
+    printf("MOUSE: key: %d, state: %d, x: %d, y: %d \n", key, state, x, y);
 }
 
 void funMotionPassive(int x, int y)
 {
-    rotY -= float(oldX - x) / 10.0f;
-    rotX -= float(oldY - y) / 10.0f;
-
-    oldY = y;
+    cameraPos[0] -= float(oldX - x) / 10.0f;
+    lookat[0] = float(oldX - x) / 10.0f;
+    cameraPos[1] += float(oldY - y) / 10.0f;
+    lookat[1] = float(oldY - y) / 10.0f;
+    
     oldX = x;
+    oldY = y;
     
     printf("PASSIVE: x: %d, y: %d \n", x, y);
 
@@ -148,16 +179,30 @@ void funMotionPassive(int x, int y)
 
 void funMotion(int x, int y)
 {
-    posX -= float(oldX - x) / 100.0f;
-    posY += float(oldY - y) / 100.0f;
-
-    oldY = y;
+    cameraPos[0] -= float(oldX - x) / 100.0f;
+    lookat[0] = float(oldX - x) / 100.0f;
+    cameraPos[1] += float(oldY - y) / 100.0f;
+    lookat[1] = float(oldY - y) / 100.0f;
+    
     oldX = x;
+    oldY = y;
 
     printf("MOTION: x: %d, y: %d \n", x, y);
 
     glutPostRedisplay();
 }
+
+void funMouseWheel(int wheel, int direction, int x, int y)
+{
+    cameraPos[2] -= direction * 0.3;
+
+    printf("MOUSEWHEEL: wheel: %d, direction: %d, x: %d, y: %d \n", wheel, direction, x, y);
+}
+
+GLfloat initialX = -2.0;
+GLfloat yellowSphereInit[3] = {0.0, 2.0, -8.0};
+GLfloat blueSphereInit[3] = {4.0, 2.0, -8.0};
+GLfloat whiteSphereInit[3] = {-1.5, 2.0, -8.0};
 
 void funDisplay() {
 
@@ -168,15 +213,30 @@ void funDisplay() {
     // Posicionamos la cámara (V)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    //gluLookAt();
 
-    GLfloat initialDist = -2.0f;
+    rotY += GLfloat(0.02);
+
+ // Posicionamos la cámara (V)
+    gluLookAt( cameraPos[0], cameraPos[1], cameraPos[2],
+                  lookat[0],    lookat[1],    lookat[2],
+                      up[0],        up[1],        up[2]);
+    
+    // Dibujamos los objetos (M)
+    glPushMatrix();
+    {
+        glTranslatef(yellowSphereInit[0], yellowSphereInit[1] - 5.0, yellowSphereInit[2]);
+        glRotatef(90.0, 0.0, 1.0, 0.0);
+        glRotatef(90.0, 1.0, 0.0, 0.0);
+        drawPlane(20.0, 20.0);
+    }
+    glPopMatrix();
 
     // Dibujamos los objetos (M)
-    /*glPushMatrix();
+    glPushMatrix();
     {
-        glTranslatef(initialDist, 0.0, -8.0);
+        glTranslatef(yellowSphereInit[0], yellowSphereInit[1], yellowSphereInit[2]);
         glRotatef(rotY, 0.0, 1.0, 0.0);
+        glRotatef(rotX, 1.0, 0.0, 0.0);
         glTranslatef(0.0, 0.0, 1.0);
         drawNumber();
     }
@@ -184,39 +244,43 @@ void funDisplay() {
 
     glPushMatrix();
     {
-        glTranslatef(initialDist, 0.0, -8.0);
+        glTranslatef(yellowSphereInit[0], yellowSphereInit[1], yellowSphereInit[2]);
         glRotatef(rotY, 0.0, 1.0, 0.0);
+        glRotatef(rotX, 1.0, 0.0, 0.0);
         drawSphere(2.0f, 'y');
     }
     glPopMatrix();
-
-    initialDist += 4.0f;
     
     glPushMatrix();
     {
-        glTranslatef(initialDist, 0.0, -8.0);
+        glTranslatef(yellowSphereInit[0], yellowSphereInit[1], yellowSphereInit[2]);
         glRotatef(rotY, 0.0, 1.0, 0.0);
+        glRotatef(rotX, 1.0, 0.0, 0.0);
+        glTranslatef(blueSphereInit[0], 0.0, 0.0);
         drawSphere(0.5f, 'b');
     }
     glPopMatrix();
     
-    initialDist += 1.5f;
-
     glPushMatrix();
     {
-        glTranslatef(initialDist, 0.0, -8.0);
+        glTranslatef(yellowSphereInit[0], yellowSphereInit[1], yellowSphereInit[2]);
         glRotatef(rotY, 0.0, 1.0, 0.0);
+        glRotatef(rotX, 1.0, 0.0, 0.0);
+        glTranslatef(blueSphereInit[0], 0.0, 0.0);
+        glRotatef(rotY, 0.0, 1.0, 0.0);
+        glRotatef(rotX, 1.0, 0.0, 0.0);
+        glTranslatef(whiteSphereInit[0], 0.0, 0.0);
         drawSphere(0.1f, 'w');
     }
     glPopMatrix();
 
-    glPushMatrix();
+    /*glPushMatrix();
     {
         glTranslatef(initialDist, 0.0, -8.0);
         glRotatef(rotY, 0.0, 1.0, 0.0);
         drawPieza(3.0f, 1.0f);
     }
-    glPopMatrix();*/
+    glPopMatrix();
     
     glPushMatrix();
     {
@@ -226,9 +290,18 @@ void funDisplay() {
         drawCube();
     }
     glPopMatrix();
+    
+    glPushMatrix();
+    {
+        glTranslatef(initialDist, 0.0, -8.0);
+        glRotatef(rotY, 0.0, 1.0, 0.0);
+        drawSphere(1.0f, 'y');
+    }
+    glPopMatrix();*/
 
     // Intercambiamos los buffers
     glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 void drawTriangulo(GLfloat s, char color) {
@@ -271,8 +344,25 @@ void drawNumber()
     glEnd();
 }
 
+void drawPlane(GLfloat width, GLfloat height)
+{
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(width, height, 0.0f);
+        glVertex3f(width, -height, 0.0f);
+        glVertex3f(-width, -height, 0.0f);
+        
+        glVertex3f(-width, -height, 0.0f);
+        glVertex3f(-width, height, 0.0f);
+        glVertex3f(width, height, 0.0f);
+    }
+    glEnd();
+}
+
 void drawSphere(GLfloat radius, char color, bool wired)
 {
+    glEnable(GL_CULL_FACE);
     switch (color)
     {
     case 'r':
@@ -294,10 +384,18 @@ void drawSphere(GLfloat radius, char color, bool wired)
 
     glLineWidth(1.0f);
 
-    if (wired)
+    glEnable(GL_POLYGON_OFFSET_FILL);
+    //glutSolidSphere(radius, 20, 20);
+
+    glDisable(GL_POLYGON_OFFSET_FILL);
+    glutWireSphere(radius, 20, 20);
+
+    /*if (wired)
         glutWireSphere(radius, 20, 20);
     else
-        glutSolidSphere(radius, 20, 20);
+        glutSolidSphere(radius, 20, 20);*/
+
+    glDisable(GL_CULL_FACE);
 }
 
 void drawPieza(GLfloat width, GLfloat height)
